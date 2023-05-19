ae1e62c7c66cb49c21c6832bf062cee510d70e7d
Evgeniy_Ustyan <prukon@gmail.com>
2023-05-20 00:05:59 +0300

    Initial Commit

---
+++ Allergen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Allergen
+//
+//  Created by Evgeniy Ustyan on 19.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
