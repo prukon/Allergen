0d46d16ab0a28f801749cc7ee4e6e78190b9f3ab
Evgeniy_Ustyan <prukon@gmail.com>
2023-05-20 00:15:23 +0300

    add logVC

---
+++ Allergen/LogInViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LogInViewController.swift
+//  Allergen
+//
+//  Created by Evgeniy Ustyan on 20.05.2023.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
